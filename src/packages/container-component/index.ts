52dd81dff443cf38a6a5a0c7eabd44c95fd46d7d
kuangyaxing <kuangyaxing@pagit.gosuncn.com>
2023-06-02 10:45:11 +0800

    types: 修复typescript编译报错

--- src/packages/container-component/index.ts
+++ src/packages/container-component/index.ts
@@ -1,12 +1,24 @@
 import { VisualEditorComponent } from '@/visual-editor/visual-editor.utils';
 
-const modules = import.meta.globEager('./*/index.tsx');
+const modules = import.meta.globEager('./*/index.tsx') as Record<
+  string,
+  undefined | { default: VisualEditorComponent } | VisualEditorComponent
+>;
+
+type DefaultModule = { default: VisualEditorComponent };
 
 const components: Record<string, VisualEditorComponent> = {};
 
 Object.keys(modules).forEach((key: string) => {
   const name = key.replace(/\.\/(.*)\/index\.(tsx|vue)/, '$1');
-  components[name] = modules[key]?.default || modules[key];
+  const module = modules[key];
+  if (module) {
+    if (Object.prototype.hasOwnProperty.call(module, 'default')) {
+      components[name] = (module as DefaultModule).default;
+    } else {
+      components[name] = module as VisualEditorComponent;
+    }
+  }
 });
 
 console.log(components, 'container-component');
